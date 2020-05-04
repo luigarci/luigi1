79ba906f5af3977e3859f46848d8ceddae91faab
Luis F Garcia <luigarci@gmail.com>
2020-05-04 17:29:12 -0400

    initial commit

---
+++ Lista_vocales.py
@@ -0,0 +1,21 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+plist2=[]
+print (plist)
+for i in range(4):
+    plist.pop()
+#plist.remove(')
+plist.pop(0)
+plist.pop(2)
+print(plist)
+for i in range(4):
+   abc =plist.pop()
+   plist2.append(abc)
+   print (abc)
+plist.insert(2," ")
+xyz=plist2.pop()
+plist2.insert(0,xyz)
+plist2.pop()
+plist.extend(plist2)
+print("plist = ", plist)
+print("plist2 =",plist2)
